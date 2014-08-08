66cb4c4635731768463e184595d873eda17f2222
Ben <ben@cheetham.me.uk>
2014-08-08 11:12:49 +0100

    Initial commit

---
+++ Models/Address.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GuildfordBoroughCouncil.Address.Models
+{
+    public enum AddressStatus
+    {
+        Active,
+        Alternative,
+        Provisional,
+        Historic,
+        Candidate,
+        Rejected
+    }
+
+    /// <summary>
+    /// Address search scope
+    /// </summary>
+    public enum AddressSearchScope
+    {
+        /// <summary>
+        /// Search the LLPG
+        /// </summary>
+        Local,
+        /// <summary>
+        /// Search AddressBase but constrain results to the authority codes listed in the LocalPlusSurroundingCodes setting
+        /// </summary>
+        LocalPlusSurrounding,
+        /// <summary>
+        /// Search AddressBase if no results are found in the LLPG
+        /// </summary>
+        National
+    }
+
+    [DisplayColumn("FullAddress")]
+    public class Address
+    {
+        public Int64? Uprn { get; set; }
+        public Int64? Usrn { get; set; }
+        [MaxLength(200)]
+        public string Organisation { get; set; }
+        [DataType(DataType.MultilineText)]
+        [MaxLength(200)]
+        public string Property { get; set; }
+        [MaxLength(200)]
+        public string Street { get; set; }
+        [MaxLength(200)]
+        public string Locality { get; set; }
+        [MaxLength(200)]
+        public string Town { get; set; }
+        [MaxLength(200)]
+        public string PostTown { get; set; }
+        [MaxLength(200)]
+        public string County { get; set; }
+        [MaxLength(10)]
+        public string PostCode { get; set; }
+        [MaxLength(200)]
+        public string Country { get; set; }
+
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+
+        public double? Northing { get; set; }
+        public double? Easting { get; set; }
+
+        public string Classification { get; set; }
+
+        public string Distance { get; set; }
+
+        public string FullAddress { get; set; }
+
+        public AddressStatus Status { get; set; }
+
+        public int? AuthorityCode { get; set; }
+        public string Authority { get; set; }
+
+        /// <summary>
+        /// Is the address in the borough for the set authority code
+        /// </summary>
+        public bool InBorough
+        {
+            get
+            {
+                return (AuthorityCode == Properties.Settings.Default.AuthorityCode);
+            }
+        }
+    }
+}
---
+++ PostCode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace GuildfordBoroughCouncil.Address
+{
+    public static class PostCode
+    {
+        internal static string FormatPostCode(Match m)
+        {
+            try
+            {
+                return m.ToString().Insert(0, " ");
+            }
+            catch
+            {
+                return m.ToString();
+            }
+        }
+
+        public static string Format(string PostCode)
+        {
+            if (!String.IsNullOrWhiteSpace(PostCode))
+            {
+                PostCode = PostCode.Trim().ToUpper();
+
+                // Remove non-alpha characters
+                PostCode = Regex.Replace(PostCode, @"\W*", String.Empty);
+
+                //[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][ABD-HJLNP-UW-Z]{2}
+                PostCode = Regex.Replace(PostCode, "[0-9][ABD-HJLNP-UW-Z]{2}", new MatchEvaluator(FormatPostCode));
+            }
+
+            return PostCode;
+        }
+    }
+}
